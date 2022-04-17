46f5107fd0d1b9ac4b737b15047727123b58d257
saibotk <git@saibotk.de>
2022-04-17 21:30:57 +0200

    update config to conform to new plugin version

--- nvim/lua/autocompletion.lua
+++ nvim/lua/autocompletion.lua
@@ -49,14 +49,14 @@ local on_attach = function(client, bufnr)
   end
 
   local opts = { noremap = true, silent = true }
-  buf_set_keymap('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts)
-  buf_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gD', '<cmd>Telescope lsp_implementations<CR>', opts)
-  buf_set_keymap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', 'gH', '<cmd>Telescope lsp_code_actions()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
-  buf_set_keymap('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+  buf_set_keymap("n", "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "gD", "<cmd>Telescope lsp_implementations<CR>", opts)
+  buf_set_keymap("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("n", "gR", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "gH", "<cmd>Telescope lsp_code_actions<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>Telescope lsp_references<CR>", opts)
+  buf_set_keymap("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
 end
 
 local lsp_installer = require 'nvim-lsp-installer'
