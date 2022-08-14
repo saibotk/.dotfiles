526df68f0fa26b4a81c44ccd4f085011ce4b08bd
Saibotk <git@saibotk.de>
2022-08-15 01:05:22 +0200

    zsh: Allow defining tools in macos/bin

--- macos/path.zsh
+++ macos/path.zsh
@@ -8,3 +8,6 @@ export PATH="$HOME/.node/bin:$PATH"
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+
+# Load dotfiles binaries for macos
+export PATH="$DOTFILES/macos/bin:$PATH"
