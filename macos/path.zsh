648732e9a7476e4b0e499dc13356b63503f50d55
Saibotk <git@saibotk.de>
2025-09-29 11:07:45 +0200

    fix(macos): Link GNU patch for composer patches
    The composer-patch util otherwise fails to apply patches.
    MacOS ships with its own version of patch and this seems to need interaction, which then causes a timeout.
    
    We now instead use GNU patch and also load it into our PATH.
    This should work everywhere and be closer to what you expect when searching for usage online.

--- macos/path.zsh
+++ macos/path.zsh
@@ -4,6 +4,9 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
 
+# Load GNU patch
+export PATH="$HOMEBREW_PREFIX/opt/gpatch/libexec/gnubin:$PATH"
+
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
