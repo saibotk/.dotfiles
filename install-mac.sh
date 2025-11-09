c8a4388ba4d0f317a19ec9ce0e7cb47b68ca2535
Saibotk <git@saibotk.de>
2025-11-09 02:37:10 +0100

    feat(proto): manage toolchains via proto (replace corepack)
    Corepack is not distributed with node anymore.
    
    Since corepack was only useful for pnpm, npm and yarn anyway, we now switched to proto.
    
    Proto is a toolchain package manager that expands upon this idea and can install many version of different package managers and other tools [0].
    
    It will autodetect the necessary version from version range requirements e.g. the package.json etc.
    
    We now use proto to manage `bun`, `node`, `pnpm` and `uv`.
    Thus these packages were removed from the Brewfile.
    Note: Node is still necessary, as it is a dependency for svgo.
    
    UPGRADING STEPS:
    - Remove system packages for `pnpm`, `bun`, `uv` and `node` (if possible)
    - On macOS: run `brew untap oven-sh/bun`
    - Remove corepack cache: `rm -rf ~/.cache/node/corepack`
    - Run `proto install -c global`
    
    0: https://moonrepo.dev/proto

--- install-mac.sh
+++ install-mac.sh
@@ -37,9 +37,11 @@ flutter --disable-analytics
 # See https://formulae.brew.sh/formula/openjdk
 sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 
-# Enable corepack
-corepack disable
-corepack enable
+# Install common package managers
+proto install node
+proto install pnpm
+proto install bun
+proto install uv
 
 # Install global Composer packages
 ${HOMEBREW_PREFIX}/bin/composer global require laravel/installer laravel/valet tightenco/takeout
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,3 +9,7 @@ export EDITOR='nvim'
 
 # Fix git log color sequences
 export LESS='-R'
+
+# proto (toolchain package manager)
+export PROTO_HOME="$XDG_DATA_HOME/proto";
+export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH";
