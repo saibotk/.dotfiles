f9df4e3029d5d522c50487d62886e0fbe1fe8e85
Saibotk <git@saibotk.de>
2026-03-29 17:53:20 +0200

    refactor(zsh): remove cat -> bat override
    This breaks some tooling and scripts.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,7 +23,6 @@ fi
 alias reloadshell="source $HOME/.zshrc"
 alias ls="eza"
 alias ll="eza -al --icons --group-directories-first"
-alias cat="bat"
 alias shrug="echo '¯\_(ツ)_/¯' | clip"
 alias c="clear"
 alias compile="commit 'compile'"
